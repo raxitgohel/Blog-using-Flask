73b1c33e3dbf3b8d9b3d8ac53594cdf266736a9f
Raxit Gohel <ragohel0101@gmail.com>
2024-02-08 18:25:57 +0530

    replaced itsdangerous with jwt

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
 from app import db, login_manager
 from flask import current_app
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from datetime import datetime
 from flask_login import UserMixin
+import jwt
+from time import time
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -15,20 +16,26 @@ class User(db.Model, UserMixin):
     profile_image = db.Column(db.String(20), nullable=False, default='default.jpeg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', back_populates='author', lazy=True)
-
-    def get_reset_token(self, expires_sec=600):
-        s = Serializer(current_app.config['SECRET_KEY'], expires_sec)
-        return s.dumps({'user_id': self.id}).decode('utf-8')
     
+    def get_reset_token(self, expires_sec=600):
+        payload = {'user_id': self.id, 'exp': time() + expires_sec}
+        token = jwt.encode(payload, current_app.config['SECRET_KEY'], algorithm='HS256')
+        return token
+        
     @staticmethod
     def verify_reset_token(token):
-        s = Serializer(current_app.config['SECRET_KEY'])
         try:
-            user_id = s.loads(token)['user_id']
-        except:
+            payload = jwt.decode(token, current_app.config['SECRET_KEY'], algorithms=['HS256'])
+            user_id = payload['user_id']
+            return User.query.get(user_id)
+        except jwt.ExpiredSignatureError:
+            # Token has expired
             return None
-        return User.query.get(user_id)
-    def __repr__(self) -> str:
+        except jwt.InvalidTokenError:
+            # Token is invalid
+            return None
+
+    def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.profile_image}')"
 
 class Post(db.Model):
