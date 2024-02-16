987dbce23bba67273390f7d524b4ea496aaf30d1
Raxit Gohel <ragohel0101@gmail.com>
2024-02-17 03:14:42 +0530

    Added search functionality

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 from app.models import Post
+from sqlalchemy import or_
 
 main = Blueprint('main', __name__)
 
@@ -14,3 +15,15 @@ def home():
 @main.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+@main.route('/search')
+def search():
+    search_query = request.args.get('query')
+    if search_query is None or search_query=="":
+        return redirect(url_for('main.home'))
+    # Perform search using SQLAlchemy
+    search_results = Post.query.filter(\
+        or_(Post.title.contains(search_query), Post.content.contains(search_query))\
+            ).order_by(Post.date_posted.desc()).all()
+    return render_template('search_results.html', posts=search_results, search_query=search_query)
+        
